cf3772d96ea2a1c9ae246628838f6a2f8c06f0b3
Merichu Duncan <chue_3@yahoo.com>
2017-02-09 09:47:00 -0500

    Added List and Task Models

---
+++ ToDo/Models/List.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ToDo.Models
+{
+    public class List
+    {
+        //Primary Key
+        //Primary Table
+        [Key]
+        public int ListID { get; set; }
+        public string Title { get; set; }
+        public DateTime Date { get; set; }
+
+        //Added cause this list would have many tasks
+        public virtual ICollection<Task> Tasks { get; set; }
+
+
+    }
+
+}
---
+++ ToDo/Models/Task.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace ToDo.Models
+{
+    public class Task
+    {
+        //Primary Key
+        //Primary Table
+        [Key]
+        public int TaskID { get; set; }
+        public string TaskName { get; set; }
+        public string Description { get; set; }
+        public bool IsDone { get; set; }
+        public DateTime Date { get; set; }
+
+
+        //Inorder to connect two classes/communicate each other
+        [ForeignKey("List")]
+        public int ListID { get; set; }
+        public virtual List List { get; set; }
+    }
+}
